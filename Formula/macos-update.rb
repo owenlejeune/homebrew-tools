f2ea0d20b0af0074f0d5383f3625b8601a54827d
Owen LeJeune <omlejeune@gmail.com>
2019-03-05 11:26:05 -0500

    Update macos-update.rb

--- Formula/macos-update.rb
+++ Formula/macos-update.rb
@@ -4,9 +4,9 @@ class MacosUpdate < Formula
     url "https://github.com/owenlejeune/macOS-update/archive/v1.0.tar.gz"
     sha256 "2f01b12015f0aafead28b7b90c4dd44190d76883a09af58a6aa0f9459256b2e7"
     
-    bottle do
-        sha256 "61b2f0f4a65f40ef38d47c552b5717e4a1c454ce85310d223f1fc011a7e246d6" => :mojave
-    end
+#     bottle do
+#         sha256 "61b2f0f4a65f40ef38d47c552b5717e4a1c454ce85310d223f1fc011a7e246d6" => :mojave
+#     end
     
     def install
         bin.install "macosupdate"
