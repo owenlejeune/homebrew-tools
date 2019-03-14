988e25ea68755c87a2a62fff4a2ec6acc01b0e23
Owen LeJeune <owenlejeune@cmail.carleton.ca>
2019-03-14 15:39:14 -0400

    Added Duo Desktop Cask

---
+++ Cask/duo-desktop.rb
@@ -0,0 +1,10 @@
+cask 'duo-desktop' do
+  version '1.0'
+  sha256 '40262eb0e0802e77f128698500dadfde8d4d1fa19670199f62978c7e6a6cdc60'
+
+  url 'https://github.com/owenlejeune/duo-desktop/releases/download/v1.0/Duo.Desktop.dmg'
+  name 'Duo Desktop'
+  homepage 'https://github.com/owenlejeune/duo-desktop'
+
+  app 'Duo Desktop.app'
+end
